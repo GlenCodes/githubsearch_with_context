3a6a19763b74955c23fa614823ad53af653e2d05
Glen Burnett <ultron@Glens-MacBook-Air.local>
2019-07-15 16:18:38 +0800

    Using React Routers

---
+++ src/components/layout/Navbar.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
+
+const Navbar = ({ icon, title }) => {
+  return (
+    <div>
+      <nav className="navbar bg-primary">
+        <h1>
+          <i className={icon} /> {title}
+        </h1>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  );
+};
+
+Navbar.defaultProps = {
+  title: 'Github Finder',
+  icon: 'fab fa-github'
+};
+
+// forces the component to ensure it has these two things, and as strings
+Navbar.propTypes = {
+  title: PropTypes.string.isRequired,
+  icon: PropTypes.string.isRequired
+};
+
+export default Navbar;
